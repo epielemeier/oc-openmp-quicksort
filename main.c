9d1005b9a7fb2eacb569568e52219e1cfcdae5bf
emilypie <empielemeier@gmail.com>
2017-12-10 14:36:33 -0600

    Recursive serial quicksort

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 
 void quicksort(int*, int);
+int partition(int*, int, int);
 
 int main(int argc, char* argv[]) {
 
@@ -21,6 +22,35 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+//Algorithm copied with minor changes from https://rosettacode.org/wiki/Sorting_algorithms/Quicksort#C
 void quicksort(int *a, int n) {
+
+    if (n < 2) return;
+
+    int pivot = a[n / 2];
+
+    int new_pivot = partition(a, pivot, n);
+
+    quicksort(a, new_pivot);
+    quicksort(a + new_pivot, n - new_pivot);
 }
 
+void swap(int *a, int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+int partition(int *a, int pivot, int n) {
+    int i, j;
+    for (i = 0, j = n - 1; ; i++, j--) {
+        while (a[i] < pivot) i++;
+        while (a[j] > pivot) j--;
+
+        if (i >= j) break;
+
+        swap(a, i, j);
+
+    }
+    return i;
+}
