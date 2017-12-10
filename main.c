4e82f7043dbc0b1e4cabfb93f05526eb580c5683
emilypie <empielemeier@gmail.com>
2017-12-10 14:16:30 -0600

    Add data set

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
-#ifndef _OPENMP
-#   include "omp.h"
-#endif
+#include "omp.h"
 #include <stdlib.h>
 
+#define NUM_ELEMENTS 10
+
+
+void quicksort(int*, int);
 
 int main(int argc, char* argv[]) {
 
+    int a[NUM_ELEMENTS];
+    int n = sizeof a / sizeof a[0];
+    for (int i = 0; i < n; i++)
+        a[i] = rand();
+
+    quicksort(a, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d\n", a[i]);
     return 0;
 }
+
+void quicksort(int *a, int n) {
+}
+
