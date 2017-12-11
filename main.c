8b389f026ef85bc9528ed6fe3a57a3e52d7f0915
emilypie <empielemeier@gmail.com>
2017-12-11 11:04:28 -0600

    Add benchmarking for different numbers of threads

--- main.c
+++ main.c
@@ -1,25 +1,56 @@
 #include <stdio.h>
 #include "omp.h"
 #include <stdlib.h>
+#include <string.h>
 
-#define NUM_ELEMENTS 10
+#define NUM_ELEMENTS 10000
 
+int* generate_random_array(int n);
+int* duplicate_array(int const * src, size_t len);
+void benchmark(int *a, int n, int thread_count);
 
 void quicksort(int*, int);
 int partition(int*, int, int);
 
 int main(int argc, char* argv[]) {
 
-    int a[NUM_ELEMENTS];
-    int n = sizeof a / sizeof a[0];
+    int *a = generate_random_array(NUM_ELEMENTS);
+    int n = NUM_ELEMENTS;
+
+    int threads_to_test = 6;
+    for (int i = 1; i <= threads_to_test; i++) {
+        int *b = duplicate_array(a, n);
+        benchmark(b, n, i);
+        free(b);
+    }
+    free(a);
+    return 0;
+}
+
+int* generate_random_array(int n) {
+    int *a = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
         a[i] = rand();
+    return a;
+}
 
+int* duplicate_array(int const * src, size_t len) {
+    int * p = malloc(len * sizeof(int));
+    memcpy(p, src, len * sizeof(int));
+    return p;
+}
+
+void benchmark(int *a, int n, int thread_count) {
+    omp_set_num_threads(thread_count);
+    double start_time = omp_get_wtime();
     quicksort(a, n);
+    double end_time = omp_get_wtime();
 
-    for (int i = 0; i < n; i++)
-        printf("%d\n", a[i]);
-    return 0;
+    for (int i = 1; i < n; i++)
+        if (a[i] < a[i-1])
+            printf("\nNot in order!");
+    printf("\nNum threads: %d\tPerformance: %f", thread_count, end_time - start_time);
+
 }
 
 //Algorithm copied with minor changes from https://rosettacode.org/wiki/Sorting_algorithms/Quicksort#C
@@ -43,6 +74,7 @@ void swap(int *a, int i, int j) {
 
 int partition(int *a, int pivot, int n) {
     int i, j;
+
     for (i = 0, j = n - 1; ; i++, j--) {
         while (a[i] < pivot) i++;
         while (a[j] > pivot) j--;
