8a8630e8ee5093b164357e264db09ae6cf0ce517
emilypie <empielemeier@gmail.com>
2017-12-11 17:31:14 -0600

    Try various sizes of arrays and thread counts, comparing the best timing of each

--- main.c
+++ main.c
@@ -3,27 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NUM_ELEMENTS 10000
+#define REPEAT_BENCHMARK 3
 
 int* generate_random_array(int n);
 int* duplicate_array(int const * src, size_t len);
-void benchmark(int *a, int n, int thread_count);
+double get_benchmark(int *a, int n, int thread_count);
+double** get_benchmarks(int *ns, int ns_size, int *thread_counts, int tc_size);
 
 void quicksort(int*, int, int);
 int partition(int*, int, int);
 
 int main(int argc, char* argv[]) {
 
-    int *a = generate_random_array(NUM_ELEMENTS);
-    int n = NUM_ELEMENTS;
+    int sizes_to_try[] = {10, 100, 1000, 10000};
+    int sizes_n = sizeof(sizes_to_try) / sizeof(sizes_to_try[0]);
+    int threads_to_try[] = {1, 2, 4, 8};
+    int threads_n = sizeof(threads_to_try) / sizeof(threads_to_try[0]);
 
-    int threads_to_test = 6;
-    for (int i = 1; i <= threads_to_test; i++) {
-        int *b = duplicate_array(a, n);
-        benchmark(b, n, i);
-        free(b);
-    }
-    free(a);
+    double **time = get_benchmarks(sizes_to_try, sizes_n, threads_to_try, threads_n);
+    deallocate_mem(time, sizes_n);
+
     return 0;
 }
 
@@ -40,7 +39,46 @@ int* duplicate_array(int const * src, size_t len) {
     return p;
 }
 
-void benchmark(int *a, int n, int thread_count) {
+double** allocate_mem(int n, int m) {
+    double **array = malloc(n * sizeof(double *));
+    for(int i = 0; i < n; i++)
+        array[i] = malloc(m * sizeof(double));
+    return array;
+}
+
+void deallocate_mem(double** arr, int n){
+    for (int i = 0; i < n; i++)
+        free(arr[i]);
+    free(arr);
+}
+
+double** get_benchmarks(int *ns, int ns_size, int *thread_counts, int tc_size) {
+
+    double **time = allocate_mem(ns_size, tc_size);
+
+    for (int i = 0; i < ns_size; i++) {
+        int n = ns[i];
+        int *a = generate_random_array(n);
+        for (int j = 0; j < tc_size; j++) {
+            int tc = thread_counts[j];
+            for (int k = 0; k < REPEAT_BENCHMARK; k++) {
+                int *b = duplicate_array(a, n);
+                if (k == 0)
+                    time[i][j] = get_benchmark(b, n, tc);
+                else {
+                    double new_time = get_benchmark(b, n, tc);
+                    if (new_time < time[i][j])
+                        time[i][j] = new_time;
+                }
+                printf("Thread count: %d   # elements: %d   Result: %f\n", tc, n, time[i][j]);
+                free(b);
+            }
+        }
+    }
+    return time;
+}
+
+double get_benchmark(int *a, int n, int thread_count) {
     omp_set_num_threads(thread_count);
     double start_time = omp_get_wtime();
     quicksort(a, n, n);
@@ -49,8 +87,8 @@ void benchmark(int *a, int n, int thread_count) {
     for (int i = 1; i < n; i++)
         if (a[i] < a[i-1])
             printf("\nNot in order!");
-    printf("\nNum threads: %d\tPerformance: %f", thread_count, end_time - start_time);
 
+    return end_time - start_time;
 }
 
 //Algorithm copied with minor changes from https://rosettacode.org/wiki/Sorting_algorithms/Quicksort#C
